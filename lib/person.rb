1d1644cc30f2153eca5dd3de19c1fae770d0a962
Njoki <kinuthiash@gmail.com>
2022-06-14 11:58:44 +0300

    commit tree a8ab7f6e874e22f6390c41b737579bbd02f7d5bf

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
-  # your code here
+  def initialize person_hash
+
+    person_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=" , value)    
+    end
+  end
 end
+
+suzie = {
+  name:"suzie", 
+  birthday:"28th Oct", 
+  hair_color:"black",
+  eye_color:"blue", 
+  height:"5'1",
+  weight:"54kgs", 
+  handed:"right", 
+  complexion:"dark-chocolate", 
+  t_shirt_size:"medium",  
+}
+
+person1 = Person.new(suzie)
+puts person1.complexion
